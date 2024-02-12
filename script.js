a6d073cc125fc97996e3ff9bac3f20fab2a81492
Ubuntu <87639130+doesDeveloper@users.noreply.github.com>
2024-02-12 16:27:31 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        const resultTag = document.getElementById('result');
+        const scoreInput = document.getElementById('score').value;
+        const categorySelect = document.getElementById('category').value;
+        const shiftSelect = document.getElementById('shift').value;
+        console.log("Hello")
+        const data = {
+            name: "a",
+            phone: "1000000000",
+            score: parseInt(scoreInput),
+            applicationNumber: "123412341234",
+            category: categorySelect,
+            shift: shiftSelect,
+            dob: "2025-02-07T00:00:00.000Z"
+          };
+        
+
+        fetch('https://api.penpencil.co/v1/student-acquisition/public/mba-percentile-predictor/jee-main', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(result => {
+            
+            if (result.success) {
+                let text= 'Percentile: '+ result.data.lowerPercentile + ' - ' + result.data.upperPercentile
+                resultTag.innerHTML = text
+            }else{
+                let text= 'Error: ' + result.error.message
+                resultTag.innerHTML = text
+
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+});
